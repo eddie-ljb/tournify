c85da83b2c4db8f96229dd7c972c722f28b05728
eddie-ljb <69401607+eddie-ljb@users.noreply.github.com>
2024-05-23 11:16:15 +0200

    initial commit

---
+++ src/main/java/de/ebader/tournify/TournifyApplication.java
@@ -0,0 +1,13 @@
+package de.ebader.tournify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ebader/tournify/TournifyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ebader.tournify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
